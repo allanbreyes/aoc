d819fbf29392d55a807d3414d1bc2fdf8845ef1d
Allan Reyes <allanbreyes@users.noreply.github.com>
2022-12-20 02:27:12 -0500

    Solve day 20.2

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,34 +1,43 @@
-pub fn part_one(input: &str) -> Option<i32> {
+pub fn part_one(input: &str) -> Option<i64> {
+    let key = 1;
     let values = &parse(input)[..];
     let mut pointers: Vec<usize> = (0..values.len()).collect();
-    mix(&values, &mut pointers);
-    Some(calculate(values, pointers))
+    mix(values, &mut pointers, 1, key);
+    Some(calculate(values, pointers, key))
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let key = 811589153;
+    let values = &parse(input)[..];
+    let mut pointers: Vec<usize> = (0..values.len()).collect();
+    mix(values, &mut pointers, 10, key);
+    Some(calculate(values, pointers, key))
 }
 
-fn mix(values: &[i32], pointers: &mut Vec<usize>) {
-    let divisor = values.len() as i32 - 1; // One less, before swapping
-    for (i, &value) in values.iter().enumerate() {
-        let index: i32 = pointers
-            .iter()
-            .position(|&p| p == i)
-            .unwrap()
-            .try_into()
-            .unwrap();
+fn mix(values: &[i64], pointers: &mut Vec<usize>, times: usize, key: i64) {
+    let divisor = values.len() as i64 - 1; // One less, before swapping
+    let values: Vec<i64> = values.iter().map(|&v| v * key).collect();
+    for _ in 0..times {
+        for (i, &value) in values.iter().enumerate() {
+            let index: i64 = pointers
+                .iter()
+                .position(|&p| p == i)
+                .unwrap()
+                .try_into()
+                .unwrap();
 
-        pointers.remove(index as usize);
-        pointers.insert(
-            // https://doc.rust-lang.org/std/primitive.i32.html#method.rem_euclid
-            (index + value).rem_euclid(divisor) as usize,
-            i,
-        )
+            pointers.remove(index as usize);
+            pointers.insert(
+                // https://doc.rust-lang.org/std/primitive.i64.html#method.rem_euclid
+                (index + value).rem_euclid(divisor) as usize,
+                i,
+            )
+        }
     }
 }
 
-fn calculate(values: &[i32], pointers: Vec<usize>) -> i32 {
+fn calculate(values: &[i64], pointers: Vec<usize>, key: i64) -> i64 {
+    let values: Vec<i64> = values.iter().map(|&v| v * key).collect();
     let zero_ptr = values.iter().position(|&v| v == 0).unwrap();
     let offset = pointers.iter().position(|&p| p == zero_ptr).unwrap();
     [1000, 2000, 3000]
@@ -37,7 +46,7 @@ fn calculate(values: &[i32], pointers: Vec<usize>) -> i32 {
         .sum()
 }
 
-fn parse(input: &str) -> Vec<i32> {
+fn parse(input: &str) -> Vec<i64> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
@@ -60,6 +69,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 20);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(1623178306));
     }
 }
