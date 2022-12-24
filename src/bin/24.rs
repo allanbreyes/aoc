d2237fa0e403c3842b31dfa0e102542188341405
Allan Reyes <allanbreyes@users.noreply.github.com>
2022-12-24 09:27:26 -0500

    Solve day 24.2

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -3,13 +3,19 @@ use std::fmt::{Debug, Formatter, Result as FmtResult};
 use pathfinding::prelude::dijkstra;
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let grid: Grid = parse(input);
-    let (_path, length) = shortest(&grid, &Pos(0, 1, 0), 300).expect("no path found");
-    Some(length)
+    let init: Grid = parse(input);
+    let grids = make_grids(&init, 300);
+    let (_, cost) = shortest(&grids, &Pos(0, 1, 0), Tile::End).expect("no path found");
+    Some(cost)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let init: Grid = parse(input);
+    let grids = make_grids(&init, 1000);
+    let (there, _) = shortest(&grids, &Pos(0, 1, 0), Tile::End).expect("no path found");
+    let (back, _) = shortest(&grids, there.last().unwrap(), Tile::Start).expect("no path found");
+    let (again, _) = shortest(&grids, back.last().unwrap(), Tile::End).expect("no path found");
+    Some(again.last().unwrap().2 as usize)
 }
 
 const UP: u8 = 0b1000;
@@ -17,22 +23,13 @@ const RIGHT: u8 = 0b0100;
 const DOWN: u8 = 0b0010;
 const LEFT: u8 = 0b0001;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Tile {
     Start,
     End,
     Wall,
     Open,
     Blizzard(u8),
-}
-
-impl Tile {
-    fn has(&self, dir: u8) -> bool {
-        match self {
-            Tile::Blizzard(b) => b & dir != 0,
-            _ => false,
-        }
-    }
 }
 
 type Grid = Vec<Vec<Tile>>;
@@ -51,81 +48,75 @@ impl Pos {
     fn successors(&self, grids: &Grids) -> Vec<(Pos, usize)> {
         let t = self.2;
 
-        // Return early if we didn't map out the grid far enough or if success
-        if t + 1 >= grids.len() || self.success(grids) {
+        // Return early if we didn't map out the grid far enough
+        if t + 1 >= grids.len() {
             return vec![];
         }
 
-        // Start with staying
-        let mut candidates: Vec<Pos> = vec![Pos(self.0, self.1, t + 1)]; // Stay
-
-        // Prune initial candidates
-        // NOTE: apparently you can just *waltz* right through a blizzard. Let's
-        // keep this here in the case that part 2 changes that.
-        const WALTZ: bool = true;
-        for (i, j, block) in [
-            (self.0 as i32 - 1, self.1 as i32, DOWN),  // Up
-            (self.0 as i32, self.1 as i32 + 1, LEFT),  // Right
-            (self.0 as i32 + 1, self.1 as i32, UP),    // Down
-            (self.0 as i32, self.1 as i32 - 1, RIGHT), // Left
-        ] {
-            if i < 0 || j < 0 {
-                continue;
+        // NOTE: apparently you can just *waltz* right through a blizzard!
+        [
+            (self.0 as i32, self.1 as i32),     // Stay
+            (self.0 as i32 - 1, self.1 as i32), // Up
+            (self.0 as i32, self.1 as i32 + 1), // Right
+            (self.0 as i32 + 1, self.1 as i32), // Down
+            (self.0 as i32, self.1 as i32 - 1), // Left
+        ]
+        .iter()
+        .filter_map(|(i, j)| {
+            if *i < 0
+                || *j < 0
+                || *i >= grids[t].len() as i32
+                || *j >= grids[t][*i as usize].len() as i32
+            {
+                return None;
             }
-            if !WALTZ && grids[t][i as usize][j as usize].has(block) {
-                continue;
+            let p = Pos(*i as usize, *j as usize, t + 1);
+            let tile = &grids[p.2][p.0][p.1];
+            if matches!(tile, Tile::Start | Tile::Open | Tile::End) {
+                Some((p, 1))
+            } else {
+                None
             }
-            candidates.push(Pos(i as usize, j as usize, t + 1));
-        }
-
-        // Filter out blocked tiles
-        candidates
-            .iter()
-            .filter_map(|p| {
-                let tile = &grids[p.2][p.0][p.1];
-                if matches!(tile, Tile::Start | Tile::Open | Tile::End) {
-                    Some((p.clone(), 1))
-                } else {
-                    None
-                }
-            })
-            .collect()
+        })
+        .collect()
     }
 
-    fn success(&self, grids: &Grids) -> bool {
+    fn success(&self, grids: &Grids, goal: Tile) -> bool {
         let tile = &grids[self.2][self.0][self.1];
-        matches!(tile, Tile::End)
+        tile == &goal
     }
 }
 
-fn shortest(init: &Grid, start: &Pos, max_t: usize) -> Option<(Vec<Pos>, usize)> {
-    let mut grids: Grids = vec![init.clone()];
-    for _ in 1..max_t {
-        let next = tick(grids.last().unwrap());
-        grids.push(next);
-    }
-
+fn shortest(grids: &Grids, start: &Pos, goal: Tile) -> Option<(Vec<Pos>, usize)> {
     let result = dijkstra(
         start,
-        |p: &Pos| p.successors(&grids),
-        |p: &Pos| p.success(&grids),
+        |p: &Pos| p.successors(grids),
+        |p: &Pos| p.success(grids, goal),
     );
 
     let debug: Option<Pos> = None;
     if let Some(pos) = debug {
         draw(&grids[pos.2], (pos.0, pos.1));
         let successors: Vec<Pos> = pos
-            .successors(&grids)
+            .successors(grids)
             .iter()
             .map(|(p, _)| p.clone())
             .collect();
-        dbg!(&successors, pos.success(&grids));
         if successors.is_empty() {
             draw(&grids[pos.2 + 1], (successors[0].0, successors[0].1));
         }
     }
 
     result
+}
+
+fn make_grids(init: &Grid, max_t: usize) -> Grids {
+    let mut grids: Grids = vec![init.clone()];
+    for _ in 1..max_t {
+        let next = tick(grids.last().unwrap());
+        grids.push(next);
+    }
+    grids
 }
 
 fn tick(grid: &Grid) -> Grid {
