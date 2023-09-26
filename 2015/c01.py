c2de7987686ac7c59feb2fb22c380d6cc4745dae
Allan Reyes <allanbreyes@users.noreply.github.com>
2023-09-25 22:13:44 -0400

    Solve day 1

--- 2015/c01.py
+++ 2015/c01.py
@@ -4,15 +4,38 @@ from typing import Optional
 class Solution:
     key = "01"
 
-    def __init__(self, input: Optional[str] = None):
-        if input is None:
-            with open(f"./inputs/{self.key}", "r") as fh:
-                self.input = fh.read()
-        else:
-            self.input = input
+    def __init__(self, input: str):
+        self.input = input
 
     def part_one(self):
-        pass
+        floor = 0
+        for char in self.input.strip():
+            match char:
+                case "(":
+                    floor += 1
+                case ")":
+                    floor -= 1
+                case _:
+                    raise ValueError("unrecognized character")
+        return floor
+
 
     def part_two(self):
-        pass
+        floor = 0
+        for i, char in enumerate(self.input.strip()):
+            match char:
+                case "(":
+                    floor += 1
+                case ")":
+                    floor -= 1
+                case _:
+                    raise ValueError("unrecognized character")
+            if floor < 0:
+                return i + 1
+
+
+if __name__ == "__main__":
+    with open(f"./inputs/{Solution.key}.txt", "r") as fh:
+        input = fh.read()
+    print("Part 1:", Solution(input).part_one())
+    print("Part 2:", Solution(input).part_two())
--- 2015/c01_test.py
+++ 2015/c01_test.py
@@ -5,11 +5,11 @@ from c01 import Solution
 
 @pytest.fixture
 def example():
-    return "(())"
+    return "(()))("
 
 def test_part_one(example):
-    assert Solution(example).part_one() == "foo"
+    assert Solution(example).part_one() == 0
 
 
 def test_part_two(example):
-    assert Solution(example).part_two() == "bar"
+    assert Solution(example).part_two() == 5
