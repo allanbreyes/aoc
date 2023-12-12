96e4c7c11999c9b397636685586e5a7fee262ebf
Allan Reyes <allanbreyes@users.noreply.github.com>
2023-12-12 07:07:39 -0500

    Solve day 12

--- 2023/d12/d12.go
+++ 2023/d12/d12.go
@@ -1,131 +1,101 @@
 package d12
 
 import (
-	"log"
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 type Record struct {
-	row  string
-	vals []int
+	row    string
+	groups []int
+	streak bool
+}
+
+func (r Record) Key() (key string) {
+	// NOTE: There's surely a better way to memoize function arguments a la
+	// Python's @cache. This ain't it.
+	return fmt.Sprintf("%s:%v:%v", r.row, r.groups, r.streak)
 }
 
 func SolvePart1(input string) (ans int) {
+	cache := map[string]int{}
 	for _, record := range Parse(input, false) {
-		ans += Enumerate(record)
+		ans += Enumerate(record, cache)
 	}
 	return
 }
 
 func SolvePart2(input string) (ans int) {
-	// TODO(perf): speed this up
-	records := Parse(input, true)
-	log.Println("processing records:", len(records))
-	for i, record := range Parse(input, true) {
-		log.Println("processing record:", i, record)
-		ans += Enumerate(record)
+	cache := map[string]int{}
+	for _, record := range Parse(input, true) {
+		ans += Enumerate(record, cache)
 	}
 	return
 }
 
-func Enumerate(record Record) (ans int) {
-	i := strings.Index(record.row, "?")
-	if i == -1 {
-		if IsValid(record, true) {
-			ans++
+func Enumerate(record Record, cache map[string]int) (ans int) {
+	// Use memoized value if available
+	key := record.Key()
+	if val, ok := cache[key]; ok {
+		return val
+	}
+
+	// Base case
+	if len(record.row) == 0 {
+		if len(record.groups) == 0 {
+			return 1
 		}
-	} else if !IsValid(record, false) {
 		return 0
-	} else {
-		ans += Enumerate(Record{strings.Replace(record.row, "?", ".", 1), record.vals})
-		ans += Enumerate(Record{strings.Replace(record.row, "?", "#", 1), record.vals})
 	}
 
+	head := record.row[0]
+	if (head == '#' || head == '?') && len(record.groups) > 0 && record.groups[0] > 0 {
+		// Continue streak
+		groups := []int{record.groups[0] - 1}
+		groups = append(groups, record.groups[1:]...)
+		ans += Enumerate(Record{record.row[1:], groups, true}, cache)
+	}
+	if head == '.' || head == '?' {
+		// Streak broken in a valid way
+		if record.streak && len(record.groups) > 0 && record.groups[0] == 0 {
+			ans += Enumerate(Record{record.row[1:], record.groups[1:], false}, cache)
+		}
+
+		// Continue
+		if !record.streak {
+			ans += Enumerate(Record{record.row[1:], record.groups, false}, cache)
+		}
+	}
+
+	cache[key] = ans
 	return ans
-}
-
-func IsValid(record Record, full bool) bool {
-	streak := 0
-	vals := record.vals
-	last := '?'
-
-	row := record.row
-	if !full {
-		j := strings.Index(row, "?")
-		if j != -1 {
-			row = row[:j]
-		}
-	}
-
-	for i, char := range row {
-		if i == 0 {
-			last = char
-		}
-		switch char {
-		case '?':
-			log.Fatalf("resolve all unknowns first")
-		case '.':
-			{
-				if last == '#' {
-					// No more streaks left
-					if len(vals) == 0 {
-						return false
-					}
-
-					// Get and check the next streak, reset if still valid
-					val := vals[0]
-					vals = vals[1:]
-					if streak != val {
-						return false
-					}
-					streak = 0
-				}
-			}
-		case '#':
-			streak++
-		}
-
-		last = char
-	}
-
-	// Bail early if it's a partial check
-	if !full {
-		return true
-	}
-
-	// If one streak remains, check it
-	if len(vals) == 1 {
-		return streak == vals[0]
-	}
-
-	// Valid if no streak or values remain
-	return len(vals) == 0 && streak == 0
 }
 
 func unfold(record Record) (new Record) {
 	strs := []string{}
-	vals := []int{}
+	groups := []int{}
 	for i := 0; i < 5; i++ {
 		strs = append(strs, string(record.row))
-		vals = append(vals, record.vals...)
+		groups = append(groups, record.groups...)
 	}
 	row := strings.Join(strs, "?")
-	return Record{row, vals}
+	return Record{row, groups, false}
 }
 
 func Parse(input string, folded bool) (records []Record) {
 	for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
 		parts := strings.Split(line, " ")
-		vals := []int{}
+		groups := []int{}
 		for _, num := range strings.Split(parts[1], ",") {
-			val, _ := strconv.Atoi(num)
-			vals = append(vals, val)
+			group, _ := strconv.Atoi(num)
+			groups = append(groups, group)
 		}
-		record := Record{parts[0], vals}
+		record := Record{parts[0], groups, false}
 		if folded {
 			record = unfold(record)
 		}
+		record.row = record.row + "." // HACK: Break streaks before last iter
 		records = append(records, record)
 	}
 	return records
--- 2023/d12/d12_test.go
+++ 2023/d12/d12_test.go
@@ -4,13 +4,6 @@ import (
 	"testing"
 )
 
-var original = `#.#.### 1,1,3
-.#...#....###. 1,1,3
-.#.###.#.###### 1,3,1,6
-####.#...#... 4,1,1
-#....######..#####. 1,6,5
-.###.##....# 3,2,1`
-
 var input = `???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
@@ -18,25 +11,11 @@ var input = `???.### 1,1,3
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1`
 
-func TestIsValid(t *testing.T) {
-	for _, record := range Parse(original, false) {
-		if !IsValid(record, true) {
-			t.Errorf("record %v is invalid", record)
-		}
-
-		for i, _ := range record.row {
-			partial := Record{record.row[:i], record.vals}
-			if !IsValid(partial, false) {
-				t.Errorf("record %v is partially invalid", partial)
-			}
-		}
-	}
-}
-
 func TestEnumerate(t *testing.T) {
+	cache := map[string]int{}
 	expected := []int{1, 4, 1, 1, 4, 10}
 	for i, record := range Parse(input, false) {
-		got := Enumerate(record)
+		got := Enumerate(record, cache)
 		want := expected[i]
 		if got != want {
 			t.Errorf("got %d, want %d (%v)", got, want, record)
