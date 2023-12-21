8aecc417b3147f2359dce5df6b5d6d82553e75af
Allan Reyes <allanbreyes@users.noreply.github.com>
2023-12-21 09:47:54 -0500

    Solve day 21.2

--- 2023/d21/d21_test.go
+++ 2023/d21/d21_test.go
@@ -16,20 +16,27 @@ var input = `...........
 .##..##.##.
 ...........`
 
-func TestWalk(t *testing.T) {
-	grid, start := parse(input)
-	cache := make(WalkCache)
-	got := len(walk(6, start, grid, cache))
+func TestSolve(t *testing.T) {
+	got := Solve(input, 6, false)
 	want := 16
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
 
-func TestSolvePart2(t *testing.T) {
-	got := SolvePart2(input)
-	want := 0
-	if got != want {
-		t.Errorf("got %d, want %d", got, want)
+func TestSolveTiled(t *testing.T) {
+	cases := map[int]int{
+		6:   16,
+		10:  50,
+		50:  1594,
+		100: 6536,
+		// 500: 167004,
+	}
+
+	for steps, want := range cases {
+		got := Solve(input, steps, true)
+		if got != want {
+			t.Errorf("for %d steps, got %d, want %d", steps, got, want)
+		}
 	}
 }
