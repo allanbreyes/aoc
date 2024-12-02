0e08e781aff9ba2d38e08461de06437bbe7a2c06
Allan Reyes <allanbreyes@users.noreply.github.com>
2024-12-02 00:23:15 -0500

    Solve day 2

---
+++ 2024/src/d02.test.ts
@@ -0,0 +1,20 @@
+import { Solver } from './d02';
+
+const example = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`;
+
+describe("d02 Solver", () => {
+  const solver = new Solver(example);
+
+  it("should solve part 1 examples", () => {
+    expect(solver.part1()).toBe(2);
+  });
+
+  it("should solve part 2 examples", () => {
+    expect(solver.part2()).toBe(4);
+  });
+});
---
+++ 2024/src/d02.ts
@@ -0,0 +1,59 @@
+import { readFileSync } from "fs";
+
+export class Solver {
+  constructor(private input: string) {}
+
+  part1() {
+    return this.parse().filter(Solver.isSafe).length;
+  }
+
+  part2() {
+    return this.parse().filter(level => {
+      if (Solver.isSafe(level)) {
+        return true;
+      }
+
+      for (let i = 0; i < level.length; i++) {
+        const copy = level.slice();
+        copy.splice(i, 1);
+        if (Solver.isSafe(copy)) {
+          return true;
+        }
+      }
+
+      return false;
+    }).length;
+  }
+
+  private static isSafe(level: number[]) {
+    let increasing: boolean;
+    if (level[0] < level[1]) {
+      increasing = true;
+    } else if (level[0] > level[1]) {
+      increasing = false;
+    } else {
+      return false;
+    }
+
+    for (let j = 1; j < level.length; j++) {
+      const a = level[j - 1];
+      const b = level[j];
+
+      if ((increasing ? a > b : a < b) || a === b || Math.abs(a - b) > 3) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private parse() {
+    return this.input.trim().split("\n").map(line => line.split(" ").map(Number));
+  }
+}
+
+if (require.main === module) {
+  const input = readFileSync("./inputs/02.txt", "utf8");
+  const solver = new Solver(input);
+  console.log(solver.part1());
+  console.log(solver.part2());
+}
