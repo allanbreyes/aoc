dca2cda73a368676bdab2a6db1fb42ee1aaf6e97
Advent of Code <allanbrobot@fastmail.com>
2025-12-13 14:03:52 -0500

    Solve day 1

---
+++ 2017/bin/main.ml
@@ -0,0 +1,43 @@
+open Aoc
+
+let () =
+  let args = Sys.argv in
+  let day = args.(1) in
+  let input_file = Printf.sprintf "inputs/%s.txt" day in
+  if not @@ Sys.file_exists input_file then
+    failwith (Printf.sprintf "Input file not found: %s" input_file);
+  let file = open_in input_file in
+  let inputs = really_input_string file (in_channel_length file) in
+  let (module Day : Day.S) =
+    match day with
+    | "01" -> (module Day01)
+    (*
+    | "02" -> (module Day02)
+    | "03" -> (module Day03)
+    | "04" -> (module Day04)
+    | "05" -> (module Day05)
+    | "06" -> (module Day06)
+    | "07" -> (module Day07)
+    | "08" -> (module Day08)
+    | "09" -> (module Day09)
+    | "10" -> (module Day10)
+    | "11" -> (module Day11)
+    | "12" -> (module Day12)
+    | "13" -> (module Day13)
+    | "14" -> (module Day14)
+    | "15" -> (module Day15)
+    | "16" -> (module Day16)
+    | "17" -> (module Day17)
+    | "18" -> (module Day18)
+    | "19" -> (module Day19)
+    | "20" -> (module Day20)
+    | "21" -> (module Day21)
+    | "22" -> (module Day22)
+    | "23" -> (module Day23)
+    | "24" -> (module Day24)
+    | "25" -> (module Day25)
+    *)
+    | _ -> failwith "invalid day"
+  in
+  Day.run inputs;
+  close_in file
